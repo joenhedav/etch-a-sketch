ad668c1174dd74433c1d865f1d867df28e969518
joenhedav <148872849+joenhedav@users.noreply.github.com>
2024-09-13 10:59:13 -0300

    Implementing the display grid function

--- script.js
+++ script.js
@@ -2,8 +2,12 @@ document.addEventListener('DOMContentLoaded', function(){
     const container = document.querySelector('.container');
 
     function displayGrid() {
-        
-
-
+        for (let i=0; i<16; i++) {
+            for (let j=0; j<16; j++) {
+                const cell = document.createElement('div');
+                container.appendChild(cell);
+            }
+        }
     }
+    displayGrid();
 });
