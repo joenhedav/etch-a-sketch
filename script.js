247c90eefe598da7b2f37029147ba1a7f427fe1a
joenhedav <148872849+joenhedav@users.noreply.github.com>
2024-09-17 11:38:35 -0300

    Add new functions

--- script.js
+++ script.js
@@ -1,13 +1,49 @@
 document.addEventListener('DOMContentLoaded', function(){
-    const container = document.querySelector('.container');
+    const container = document.querySelector(".container");
+    const clear = document.querySelector("#clear");
+    const resize = document.querySelector("#resize");
 
-    function displayGrid() {
-        for (let i=0; i<16; i++) {
-            for (let j=0; j<16; j++) {
-                const cell = document.createElement('div');
-                container.appendChild(cell);
-            }
+    function createCell(size) {
+        const cell = document.createElement("div");
+        cell.classList.add("cell");
+        cell.style.width = `calc(100% / ${size})`;
+        cell.style.height = `calc(100% / ${size})`;
+        cell.addEventListener("mouseover", function() {
+            changeCellColor(cell);
+        });
+        return cell;
+    }
+
+    function changeCellColor(cell) {
+        cell.style.backgroundColor = "#212123";
+    }
+
+    function displayGrid(size) {
+        container.innerHTML = '';
+
+        for (let i = 0; i < size * size; i++) {
+            const cell = createCell(size);
+            container.appendChild(cell);
         }
     }
-    displayGrid();
+
+    function clearDraw() {
+        const cells = document.querySelectorAll(".cell");
+        cells.forEach(function(cell) {
+            cell.style.backgroundColor = "#dad4c9";
+        });
+    }
+
+    clear.addEventListener("click", clearDraw);
+
+    resize.addEventListener("click", function() {
+        let size = +prompt("Enter the new grid size: ");
+        if (size > 0) {
+            displayGrid(size);
+        } else {
+            alert("Enter a valid number");
+        }
+    });
+
+    displayGrid(16);
 });
